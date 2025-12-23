cc923a844acafe338ec64ab065c2b4bb52ee5172
kimywann <kimywan10@gmail.com>
2025-12-23 20:56:41 +0900

    feat: FixedBottomCTA 공통 컴포넌트 구현

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -1,21 +1,15 @@
-import CustomButton from '@/components/CustomButton';
+import FixedBottomCTA from '@/components/FixedBottomCTA';
 import InputField from '@/components/InputField';
-import { colors } from '@/constants';
 import { StyleSheet, View } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function LoginScreen() {
-  const inset = useSafeAreaInsets();
-
   return (
     <>
       <View style={styles.container}>
         <InputField label="이메일" placeholder="이메일을 입력해주세요." />
         <InputField label="비밀번호" placeholder="비밀번호를 입력해주세요." />
       </View>
-      <View style={[styles.fixed, { paddingBottom: inset.bottom || 12 }]}>
-        <CustomButton label="로그인하기" />
-      </View>
+      <FixedBottomCTA label="로그인하기" onPress={() => {}} />
     </>
   );
 }
@@ -25,14 +19,5 @@ const styles = StyleSheet.create({
     flex: 1,
     margin: 16,
     gap: 16,
-  },
-  fixed: {
-    position: 'absolute',
-    bottom: 0,
-    width: '100%',
-    borderTopWidth: StyleSheet.hairlineWidth,
-    borderTopColor: colors.GRAY_300,
-    paddingTop: 12,
-    paddingHorizontal: 16,
   },
 });
--- app/auth/signup.tsx
+++ app/auth/signup.tsx
@@ -1,12 +1,8 @@
-import CustomButton from '@/components/CustomButton';
+import FixedBottomCTA from '@/components/FixedBottomCTA';
 import InputField from '@/components/InputField';
-import { colors } from '@/constants';
 import { StyleSheet, View } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function LoginScreen() {
-  const inset = useSafeAreaInsets();
-
   return (
     <>
       <View style={styles.container}>
@@ -14,9 +10,7 @@ export default function LoginScreen() {
         <InputField label="비밀번호" placeholder="비밀번호를 입력해주세요." />
         <InputField label="비밀번호 확인" placeholder="비밀번호를 입력해주세요." />
       </View>
-      <View style={[styles.fixed, { paddingBottom: inset.bottom || 12 }]}>
-        <CustomButton label="회원가입 하기" />
-      </View>
+      <FixedBottomCTA label="회원가입 하기" onPress={() => {}} />
     </>
   );
 }
@@ -26,14 +20,5 @@ const styles = StyleSheet.create({
     flex: 1,
     margin: 16,
     gap: 16,
-  },
-  fixed: {
-    position: 'absolute',
-    bottom: 0,
-    width: '100%',
-    borderTopWidth: StyleSheet.hairlineWidth,
-    borderTopColor: colors.GRAY_300,
-    paddingTop: 12,
-    paddingHorizontal: 16,
   },
 });
