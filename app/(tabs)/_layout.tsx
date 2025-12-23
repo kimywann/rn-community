f45e183659d7437a264589fbee253e5808eb4bf7
kimywann <kimywan10@gmail.com>
2025-12-23 11:11:02 +0900

    feat: 하단 탭 홈 아이콘 활성 상태에 따라 변경

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,5 @@
 import { colors } from '@/constants';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
@@ -13,18 +14,27 @@ export default function TabLayout() {
         name="index"
         options={{
           title: '홈',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} size={25} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="my"
         options={{
           title: '내 프로필',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'person-circle' : 'person-circle-outline'} size={25} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="setting"
         options={{
           title: '설정',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'settings' : 'settings-outline'} size={25} color={color} />
+          ),
         }}
       />
     </Tabs>
