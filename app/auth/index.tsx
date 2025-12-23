21cb48c6822d8c144cfe9308f5ac1615349a951b
kimywann <kimywan10@gmail.com>
2025-12-23 16:50:38 +0900

    feat: Auth 스크린 마크업 구현

---
+++ app/auth/index.tsx
@@ -0,0 +1,46 @@
+import CustomButton from '@/components/CustomButton';
+import { colors } from '@/constants';
+import { Link, router } from 'expo-router';
+import { Image, StyleSheet, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function AuthScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={require('@/assets/images/logo.png')} style={styles.logo} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <CustomButton label="email로 로그인" onPress={() => router.push('/auth/login')} />
+        <Link href="/" style={styles.signupText}>
+          이메일로 가입하기
+        </Link>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  imageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 2,
+  },
+  logo: {
+    width: 112,
+    height: 112,
+  },
+  buttonContainer: {
+    paddingHorizontal: 32,
+    flex: 1,
+  },
+  signupText: {
+    textAlign: 'center',
+    marginTop: 20,
+    textDecorationLine: 'underline',
+    color: colors.ORANGE_600,
+  },
+});
